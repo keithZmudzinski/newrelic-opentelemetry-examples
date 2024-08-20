b0054ac33a51c6e1a143419e1f97a54b08c87b01
Keith Zmudzinski <kzmudzinsk@wiley.com>
2024-08-20 09:26:50 -0700

    manually instrument traces

--- getting-started-guides/python/app.py
+++ getting-started-guides/python/app.py
@@ -1,12 +1,35 @@
-from flask import Flask, jsonify, request
 import logging
 
+from flask import Flask, jsonify, request
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# Service name is required for most backends
+resource = Resource(attributes = {
+    SERVICE_NAME: "getting-started-python-not-env"
+})
+
+traceProvider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter())
+traceProvider.add_span_processor(processor)
+trace.set_tracer_provider(traceProvider)
+
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer("my.tracer.name")
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
+@app.route("/not_instrumented")
+def not_instrumented():
+    return {'success': True}
+
 @app.route("/fibonacci")
+@tracer.start_as_current_span("fibonacci")
 def fibonacci():
     args = request.args
     x = int(args.get("n"))
